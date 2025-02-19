90128f91daf216765f30f4012c588c4a6a4c1e44
Isabella Lulamoon <kawapure@gmail.com>
2025-02-19 16:37:18 -0700

    Make FFI in control preview private
    Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>

--- control_preview/src/uxthemehook.rs
+++ control_preview/src/uxthemehook.rs
@@ -1,23 +1,25 @@
 //use crate::uxthemehook::{InitializeHooks};
 use winsafe::WString;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+mod ffi {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
 
-pub fn initialize_hooks() -> Result<(), String> {
+pub fn initialize_hooks() -> Result<(), &'static str> {
     unsafe {
-        match InitializeHooks() {
-            _INITRESULT_CPIR_SUCCEEDED => Ok(()),
-            _INITRESULT_CPIR_FAILED => Err("Failed to initialize hooks.".to_owned()),
-            _INITRESULT_CPIR_FAILED_MINHOOK_INIT => Err("Failed to initialize MinHook.".to_owned()),
-            _INITRESULT_CPIR_FAILED_MINHOOK_HOOK => Err("Failed to install hook.".to_owned()),
-            _ => Err("Failed to initialize hooks.".to_owned())
+        match ffi::InitializeHooks() {
+            ffi::_INITRESULT_CPIR_SUCCEEDED => Ok(()),
+            ffi::_INITRESULT_CPIR_FAILED => Err("Failed to initialize hooks."),
+            ffi::_INITRESULT_CPIR_FAILED_MINHOOK_INIT => Err("Failed to initialize MinHook."),
+            ffi::_INITRESULT_CPIR_FAILED_MINHOOK_HOOK => Err("Failed to install hook."),
+            _ => Err("Failed to initialize hooks.")
         }
     }
 }
 
 pub fn load_global_theme(theme: &str) -> Option<()> {
-    unsafe {        
-        return if LoadGlobalTheme(WString::from_str(theme).as_ptr()) >= 0 {
+    unsafe {
+        return if ffi::LoadGlobalTheme(WString::from_str(theme).as_ptr()) >= 0 {
             // HRESULT succeeded
             Some(())
         }
