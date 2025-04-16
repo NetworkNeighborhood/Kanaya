832dacfdfa53f0d64fb8527bbc6e86a0bdfd1099
Isabella Lulamoon <kawapure@gmail.com>
2025-04-15 21:24:59 -0700

    Extend use of Fluent i18n, fix FTL file.
    Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>

--- main_gui/localization/en-US/main_window.ftl
+++ main_gui/localization/en-US/main_window.ftl
@@ -3,18 +3,18 @@
 # allows adding a right-aligned text section to menu items (often used to display keyboard
 # shortcuts).
 menu-file = &File
-    # .new = &New..
-    # .open = &Open{"\u0009"}Ctrl+O
-    # .save = &Save{"\u0009"}Ctrl+S
-    # .save-as = Save &As...{"\u0009"}Ctrl+Shift+S
-    # .exit = E&xit{"\u0009"}Alt+F4
+menu-file-new = &New..
+menu-file-open = &Open{"\u0009"}Ctrl+O
+menu-file-save = &Save{"\u0009"}Ctrl+S
+menu-file-save-as = Save &As...{"\u0009"}Ctrl+Shift+S
+menu-file-exit = E&xit{"\u0009"}Alt+F4
     
 menu-edit = &Edit
-    # .cut = C&ut{"\u0009"}Ctrl+X
-    # .copy = &Copy{"\u0009"}Ctrl+C
-    # .paste = &Paste{"\u0009"}Ctrl+V
+menu-edit-cut = C&ut{"\u0009"}Ctrl+X
+menu-edit-copy = &Copy{"\u0009"}Ctrl+C
+menu-edit-paste = &Paste{"\u0009"}Ctrl+V
 
 menu-windows = &Windows
 
 menu-help = &Help
-    # .about = &About { $kanaya-brand-name }
+menu-help-about = &About { $kanaya-brand-name }
--- main_gui/src/main_window.rs
+++ main_gui/src/main_window.rs
@@ -2,7 +2,7 @@ use winsafe::{
     gui, prelude::*, AnyResult, MenuItem, HMENU, seq_ids, co,
 };
 
-use fluent::{FluentBundle, FluentResource};
+use fluent::{FluentArgs, FluentBundle, FluentResource};
 use kanaya_common::fluent_resource_manager::FluentResourceManager;
 use fluent_fallback::{Localization};
 use unic_langid::langid;
@@ -18,14 +18,15 @@ seq_ids!(
     IDM_ABOUT
 );
 
+type I18nBundle = fluent_fallback::Bundles<FluentResourceManager>;
+
 pub struct MainWindow {
     wnd: gui::WindowMain,
-    fluent_bundles: Rc<fluent_fallback::Bundles<FluentResourceManager>>,
+    fluent_bundles: Rc<I18nBundle>,
 }
 
 impl MainWindow {
     pub fn new() -> Self {
-        // fluent_resmgr STILL USES HARDCODED PATH "./tests/resources".
         let resource_manager = FluentResourceManager::new("./locale/{locale}/{res_id}".to_string());
         
         let loc = Localization::with_env(
@@ -39,7 +40,7 @@ impl MainWindow {
         
         let wnd = gui::WindowMain::new(
             gui::WindowMainOpts {
-                title: env!("KANAYA_NAME_DISPLAY").to_owned(),
+                title: env!("KANAYA_NAME_DISPLAY").to_string(),
                 size: (900, 600),
                 menu: Self::create_menu(fluent_bundles.clone()),
                 style: co::WS::CAPTION | co::WS::SYSMENU | co::WS::CLIPCHILDREN | co::WS::BORDER | co::WS::VISIBLE | co::WS::SIZEBOX | co::WS::MAXIMIZEBOX | co::WS::MINIMIZEBOX,
@@ -56,61 +57,62 @@ impl MainWindow {
         new_self
     }
     
-    pub fn create_menu(i18n: Rc<fluent_fallback::Bundles<FluentResourceManager>>) -> HMENU {
+    pub fn create_menu(i18n: Rc<I18nBundle>) -> HMENU {
         let menu: HMENU = HMENU::CreateMenu().unwrap();
         
         let mut i18n_errors = vec![];
         
-        if let Ok(file_text) = i18n.format_value_sync("menu-file", None, &mut i18n_errors) {
-            winsafe::HWND::NULL.MessageBox(&file_text.as_ref().unwrap().to_string(), "Test", winsafe::co::MB::OK);
-            
-            menu.append_item(&[
-                MenuItem::Submenu(&Self::create_menu_file(), &file_text.as_ref().unwrap().to_string()),
-                MenuItem::Submenu(&Self::create_menu_edit(), "&Edit"),
-                MenuItem::Submenu(&Self::create_menu_windows(), "&Windows"),
-                MenuItem::Submenu(&Self::create_menu_help(), "&Help"),
-            ]).expect("Failed to create menu.");
-        }
-        else {
-            winsafe::HWND::NULL.MessageBox("Internal i18n error.", "Le troll", winsafe::co::MB::OK).unwrap();
-        }
+        menu.append_item(&[
+            MenuItem::Submenu(&Self::create_menu_file(i18n.clone()), &i18n.format_value_sync("menu-file", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&File"))).as_ref().unwrap().to_string()),
+            MenuItem::Submenu(&Self::create_menu_edit(i18n.clone()), &i18n.format_value_sync("menu-edit", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&Edit"))).as_ref().unwrap().to_string()),
+            MenuItem::Submenu(&Self::create_menu_windows(i18n.clone()), &i18n.format_value_sync("menu-windows", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&Windows"))).as_ref().unwrap().to_string()),
+            MenuItem::Submenu(&Self::create_menu_help(i18n.clone()), &i18n.format_value_sync("menu-help", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&Help"))).as_ref().unwrap().to_string()),
+        ]).expect("Failed to create menu.");
         
         menu
     }
     
-    pub fn create_menu_file() -> HMENU {
+    pub fn create_menu_file(i18n: Rc<I18nBundle>) -> HMENU {
         let menu: HMENU = HMENU::CreateMenu().unwrap();
         
+        let mut i18n_errors = vec![];
+        
         menu.append_item(&[
-            MenuItem::Entry(IDM_OPEN, "&New.."),
-            MenuItem::Entry(IDM_OPEN, "&Open\tCtrl+O"),
-            MenuItem::Entry(IDM_SAVE, "&Save\tCtrl+S"),
-            MenuItem::Entry(IDM_SAVE, "Save &As...\tCtrl+Shift+S"),
+            MenuItem::Entry(IDM_OPEN, &i18n.format_value_sync("menu-file-new", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&File"))).as_ref().unwrap().to_string()),
+            MenuItem::Entry(IDM_OPEN, &i18n.format_value_sync("menu-file-open", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&File"))).as_ref().unwrap().to_string()),
+            MenuItem::Entry(IDM_SAVE, &i18n.format_value_sync("menu-file-save", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&File"))).as_ref().unwrap().to_string()),
+            MenuItem::Entry(IDM_SAVE, &i18n.format_value_sync("menu-file-save-as", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&File"))).as_ref().unwrap().to_string()),
             MenuItem::Separator,
-            MenuItem::Entry(IDM_EXIT, "E&xit\tAlt+F4"),
+            MenuItem::Entry(IDM_EXIT, &i18n.format_value_sync("menu-file-exit", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&File"))).as_ref().unwrap().to_string()),
         ]).expect("Failed to create File menu.");
         
         menu
     }
     
-    pub fn create_menu_edit() -> HMENU {
+    pub fn create_menu_edit(i18n: Rc<I18nBundle>) -> HMENU {
         let menu: HMENU = HMENU::CreateMenu().unwrap();
         
+        let mut i18n_errors = vec![];
+        
         menu.append_item(&[
-            MenuItem::Entry(0, "C&ut\tCtrl+X"),
-            MenuItem::Entry(0, "&Copy\tCtrl+C"),
-            MenuItem::Entry(0, "&Paste\tCtrl+V"),
+            MenuItem::Entry(0, &i18n.format_value_sync("menu-edit-cut", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&File"))).as_ref().unwrap().to_string()),
+            MenuItem::Entry(0, &i18n.format_value_sync("menu-edit-copy", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&File"))).as_ref().unwrap().to_string()),
+            MenuItem::Entry(0, &i18n.format_value_sync("menu-edit-paste", None, &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&File"))).as_ref().unwrap().to_string()),
             MenuItem::Separator,
+            // Placeholder item:
             MenuItem::Entry(0, "Create new class"),
         ]).expect("Failed to create Edit menu.");
         
         menu
     }
     
-    pub fn create_menu_windows() -> HMENU {
+    pub fn create_menu_windows(i18n: Rc<I18nBundle>) -> HMENU {
         let menu: HMENU = HMENU::CreateMenu().unwrap();
         
+        //let mut i18n_errors = vec![];
+        
         menu.append_item(&[
+            // Placeholder items:
             MenuItem::Entry(0, "D&ock all"),
             MenuItem::Entry(0, "D&etach all"),
         ]).expect("Failed to create Windows menu.");
@@ -118,11 +120,16 @@ impl MainWindow {
         menu
     }
     
-    pub fn create_menu_help() -> HMENU {
+    pub fn create_menu_help(i18n: Rc<I18nBundle>) -> HMENU {
         let menu: HMENU = HMENU::CreateMenu().unwrap();
         
+        let mut i18n_errors = vec![];
+        
+        let mut cut_i18n_args = FluentArgs::new();
+        cut_i18n_args.set("kanaya-brand-name", env!("KANAYA_NAME_DISPLAY"));
+        
         menu.append_item(&[
-            MenuItem::Entry(IDM_ABOUT, &format!("&About {}", env!("KANAYA_NAME_DISPLAY"))),
+            MenuItem::Entry(IDM_ABOUT, &i18n.format_value_sync("menu-help-about", Some(&cut_i18n_args), &mut i18n_errors).unwrap_or(Some(Cow::Borrowed("&File"))).as_ref().unwrap().to_string()),
         ]).expect("Failed to create Help menu.");
         
         menu
